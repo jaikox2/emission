774adc1aa95a27089216c400a24924c1c6a28f9c
pang dev <pangdev58@gmail.com>
2021-11-11 21:26:52 +0700

    upload excel file

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -2,10 +2,27 @@ const router = require('express').Router();
 
 const {
   imageUpload,
+  excelUpload,
 } = require('../services/upload');
 
 router.post('/profile/upload',
   imageUpload.single('image'), (req, res, next) => {
+    try {
+      // check file exists
+      if (!req.file) {
+        const err = new Error('Not found that file');
+        return next(err);
+      }
+
+      return res.status(200).send(req.file.filename);
+    } catch (error) {
+      error.status = 500;
+      return next(error);
+    }
+  });
+
+router.post('/excel/upload',
+  excelUpload.single('file'), (req, res, next) => {
     try {
       // check file exists
       if (!req.file) {
--- src/services/upload/index.js
+++ src/services/upload/index.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const Storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './public/uploads');
+    cb(null, './public/uploads/images');
   },
   filename(req, file, cb) {
     cb(null, `${file.fieldname}-${Date.now()}.${file.mimetype.split('/')[1]}`);
@@ -24,6 +24,33 @@ const imageUpload = multer({
   },
 });
 
+// excels
+
+const StorageExcel = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './public/uploads/excels');
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}.${file.originalname.split('.')[1]}`);
+  },
+});
+
+function mimetypeExcelChecker(mimetype) {
+  return ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'].includes(mimetype);
+}
+
+const excelUpload = multer({
+  storage: StorageExcel,
+  fileFilter: (req, file, cb) => {
+    if (mimetypeExcelChecker(file.mimetype)) {
+      return cb(null, true);
+    }
+    cb(null, false);
+    return cb(new Error('Only .xlsx format allowed!'));
+  },
+});
+
 module.exports = {
   imageUpload,
+  excelUpload,
 };
