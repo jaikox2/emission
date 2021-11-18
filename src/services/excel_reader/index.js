1ffd9dcf41384d4014e164c8e4b9508b097b36af
pang dev <pangdev58@gmail.com>
2021-11-18 18:04:12 +0700

    response excel headers

--- src/services/excel_reader/index.js
+++ src/services/excel_reader/index.js
@@ -4,25 +4,26 @@ const readXlsxFile = require('read-excel-file/node');
 async function getDataInSheet(file_name, sheetName) {
   return new Promise((resolve) => {
     readXlsxFile(`./public/uploads/excels/${file_name}`, { sheet: sheetName }).then((data) => {
-      const headers = data[0];
+      let headers = data[0];
       const source = data.slice(1);
       const sourceInObject = source.map((item) => (
         item.reduce((total, cerrent, index) => {
-          if (Number.isInteger(headers[index])) {
-            // total.data.push({
-            //   year: headers[index],
-            //   value: cerrent,
-            // });
-            // eslint-disable-next-line no-param-reassign
-            total[headers[index].toString()] = cerrent;
-          } else {
-            // eslint-disable-next-line no-param-reassign
-            total[headers[index]] = cerrent;
-          }
+          // if (Number.isInteger(headers[index])) {
+          // total.data.push({
+          //   year: headers[index],
+          //   value: cerrent,
+          // });
+          // eslint-disable-next-line no-param-reassign
+          // total[headers[index]] = cerrent;
+          // } else {
+          // eslint-disable-next-line no-param-reassign
+          total[headers[index]] = cerrent;
+          // }
           return total;
         }, {})
       ));
-      resolve(sourceInObject);
+      headers = headers.map((header) => header.toString());
+      resolve({ headers, values: sourceInObject });
     });
   });
 }
